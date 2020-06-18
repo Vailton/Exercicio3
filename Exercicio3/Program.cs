14d2231af31956b6c81f2d97ff12d8f3f5ce4671
Vailton Gomes <governador.adm@gmail.com>
2020-06-17 22:12:12 -0300

    Exercicio3

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte id;
+            byte sl;
+
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Seu id: ");
+
+            id = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantas horas trabalhou no mês:  ");
+
+            sl = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine(salario.ToString("F2", CultureInfo.InvariantCulture));            
+
+            Console.WriteLine("Seu salario é" + (id * sl) * 2);
+
+            //Console.Write(pontoFlutuante.ToString("F2", CultureInfo.InvariantCulture));
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
